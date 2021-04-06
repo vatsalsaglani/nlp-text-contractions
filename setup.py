16465d723551107d3af096575f24669b860bd50b
vatsals <vatsals@quinnox.com>
2021-04-06 21:34:38 +0530

    changed version to 0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "NLPContractions",
-    version = "v.0.0.1",
+    version = '0.1',
     url = "https://github.com/vatsalsaglani/nlp-text-contractions.git",
     description = "A Python package to replace some english words with their shorter versions (contractions) in a sentence to help text augmentation in NLP tasks",
     author = "Vatsal Saglani",
